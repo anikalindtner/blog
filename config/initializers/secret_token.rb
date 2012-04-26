543d7892ab5371ccd086f5dfbd1c3f27aa9c94cf
anika lindtner <ani@sheldon.(none)>
2012-04-26 23:38:05 +0200

    first step scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ea9cad6cd12a2858966bdb8285bb7eb3099a768a770ff78f335c7548e46bbeecc485b82a59ae140e23347225586250c78d44d9031f0b6ae99c0061b4ff40bb09'
